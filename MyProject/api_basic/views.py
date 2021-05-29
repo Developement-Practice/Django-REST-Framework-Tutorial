f709c5a298c198fe865e1789f9ace02c4b11a7e8
Kaustav Ghosh <teetangh@gmail.com>
2021-05-29 19:18:15 +0530

    started Generic Views and Mixins

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-
+from rest_framework import generics, mixins
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -17,6 +17,31 @@ from .serializers import ArticleSerializer
 # This won't work because the SessionAuthentication enforces the csrf check anyway.
 
 # Create your views here.
+
+######################### GENERIC VIEWS #########################
+
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = "id"
+
+    def get(self, request, id=None):
+
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 ######################### CLASS BASED SERIALIZERS #########################
 
