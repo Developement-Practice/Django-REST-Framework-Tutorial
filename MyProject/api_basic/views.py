83442924f616ac181a98a3867c32ca23f85955ab
Kaustav Ghosh <teetangh@gmail.com>
2021-05-29 19:35:11 +0530

    started Django TokenAuthentication, SessionAuthentication, BasicAuthentication

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -26,6 +28,9 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
     queryset = Article.objects.all()
 
     lookup_field = "id"
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id=None):
 
