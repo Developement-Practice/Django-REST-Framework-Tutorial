20278e3593f78167ade5f09e0fc7b447c2b02ba3
Kaustav Ghosh <teetangh@gmail.com>
2021-05-29 18:58:20 +0530

    started class based serializers

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -16,7 +18,70 @@ from .serializers import ArticleSerializer
 
 # Create your views here.
 
+######################### CLASS BASED SERIALIZERS #########################
 
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        # return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            # return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+            # article = Article.objects.get(pk=pk)
+
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+            # return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+
+        serializer = ArticleSerializer(article)
+        # return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+
+        # data = JSONParser().parse(request)
+        # serializer = ArticleSerializer(article, data=data)
+        serializer = ArticleSerializer(article, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            # return JsonResponse(serializer.data)
+            return Response(serializer.data)
+        # return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+
+        article.delete()
+        # return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+######################### FUNCTION BASED SERIALIZERS #########################
 # @csrf_exempt
 @api_view(["GET", "POST"])
 def article_list(request):
